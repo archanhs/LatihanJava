5d66370f93099e77b4b5ad9840c92bed45be5980
archanhs <archanhabib11@gmail.com>
2022-11-08 10:22:23 +0700

    update

---
+++ javafundamental1/src/main/java/com/bcafinance/training/day2/CekClass.java
@@ -0,0 +1,14 @@
+package com.bcafinance.training.day2;
+
+/*
+@Author ASUS a.k.a. Archan
+ITDP 7
+Created with Eclipse version 4.25.0
+Created on 08-11-2022 10:21:20
+Last Modified on 08-11-2022 10:21:20
+Version 1.0
+*/
+
+public class CekClass {
+
+}
